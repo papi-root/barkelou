0fe84b4380c76537f21790b367450c0c1a1d0f8d
papi-root <papemorlo@hotmail.fr>
2020-07-18 03:47:00 +0000

    Updates

---
+++ precache-manifest.4e49bdf9112a938387c04a488801aff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54627dde1ef2e92a36eaf696e38b2206",
+    "url": "/barkelou/index.html"
+  },
+  {
+    "revision": "2c7fe11ddab31b288713",
+    "url": "/barkelou/static/js/2.82020f1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barkelou/static/js/2.82020f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c659d1ba24e4af4fc53",
+    "url": "/barkelou/static/js/main.237a07da.chunk.js"
+  },
+  {
+    "revision": "eca3c52db83eed0fdd89",
+    "url": "/barkelou/static/js/runtime-main.cf3f7f35.js"
+  }
+]);
