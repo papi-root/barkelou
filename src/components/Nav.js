a8af744f0c6f5c55759508cae9f079316ecabfca
papi-root <papemorlo@hotmail.fr>
2020-07-23 03:35:35 +0000

    second commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,12 +13,12 @@ const Nav = () => {
                 </div> 
                 
                 <div class="logo">
-                    <h4> <Link href="/barkelou">Barkelou</Link>  </h4>
+                    <h4> <Link to="/barkelou">Barkelou</Link>  </h4>
                 </div> 
                 
                 <ul class="ul1">
                 <li><Link to="/barkelou/lancer"> Lancer votre projet</Link></li>
-                <li><Link href="/barkelou/decouverte"> Decouvrez les projets</Link></li>
+                <li><Link to="/barkelou/decouverte"> Decouvrez les projets</Link></li>
                 </ul> 
 
                 <ul class="ul2">
